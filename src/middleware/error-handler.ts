974eaab40b7a32925f78fa96dbf54eb0eff3c3fe
Gemechu Alemu <alemugemechu44@gmail.com>
2026-05-05 12:01:40 +0300

    chore: add express api foundation

---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,17 @@
+import type { NextFunction, Request, Response } from "express";
+
+export function errorHandler(
+  err: unknown,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  console.error("Unhandled request error", err);
+
+  res.status(500).json({
+    error: {
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An unexpected error occurred.",
+    },
+  });
+}
